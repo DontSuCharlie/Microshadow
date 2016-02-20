cc6dcea4760606b5dc17de81db81a4faa47e49e8
Charlie Su <Charlie Su>
2016-02-20 00:28:44 -0500

    Finished adding Microsoft Band dependencies

--- app/src/main/java/hackillinois2016/microshadow/MainActivity.java
+++ app/src/main/java/hackillinois2016/microshadow/MainActivity.java
@@ -3,6 +3,11 @@ package hackillinois2016.microshadow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.ConnectionState;
 
 
 public class MainActivity extends AppCompatActivity {
