18f7428f92690cc552cdc9b98b281d14b4bbd930
Charlie Su <Charlie Su>
2016-02-21 17:51:19 -0500

    Finished js code for streaming and writing png files, finished python code for facial recoginition, and finished linking the two together. The only things left is to actually get the direction guidance code working, testing, and making sure everything's at least decently not inefficient

--- ardrone.js
+++ ardrone.js
@@ -1,18 +1,43 @@
+/*Initialization*/
+const childProcess = require('child_process'), pythonFaceDetection;
+pythonFaceDetection = childProcess.exec("python C:/Users/drago/Github/Microshadow/detectTheFace.py", function(code){
+    if(code!=null)
+        console.log("Error: Failed to launch python script bc of:" + code);
+});
+var fs = require('fs');
+var http = require('http');
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
-/*client.config('video:video_channel', 3);
-var http = require('http');
+var lastTime = (new Date()).getTime();
+var period = 1;
+//sets the video stream we get to the bottom facing camera
+client.config('video:video_channel', 3);
+//makes the drone fly higher by setting its max altitutde to something really big
+client.config('control:altitude_max', 100000);
 var pngStream = client.getPngStream();
 console.log('Connecting png stream ...');
-client.config('control:altitude_max', 100000);
+//launch
 client.takeoff();
+client.after(1000, function(){
+    this.land();
+});
 
-function pictureSave () {
-	var lastPng;
+function savePng () {
+    var lastPng;
 	pngStream
   	.on('error', console.log)
   	.on('data', function(pngBuffer) {
-    	lastPng = pngBuffer;
+        var currentTime = (new Date()).getTime();
+        if(currentTime - lastTime > period)
+        {
+            lastTime = currentTime;
+            fs.writeFile("frame.png", pngBuffer, function(error)
+            {
+                if(error)
+                    console.log("Couldn't write to PNG because:" + error);
+            });
+        }
+        lastPng = pngBuffer;        
   	});
 
 var server = http.createServer(function(req, res) {
@@ -27,31 +52,26 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(8080, function() {
-  console.log('Serving latest png on port 8080 ...');
+    console.log('Serving latest png on port 8080 ...');
 });
 }
 
-pictureSave();
+savePng();
 
-*/
-client
+
+/*client
  .after(7500, function() {
  	this.clockwise(0.5);
  })
- //.after(3000, function() {
- 	//this.animate('flipLeft', 15);
- 	//this.animateLeds('blinkRed',5,2);
- //})
-
+ .after(3000, function() {
+ 	this.animate('flipLeft', 15);
+ 	this.animateLeds('blinkRed',5,2);
+ })
   .after(1000, function() {
  	this.stop();
  	this.land();
- });
- client.on('navdata', console.log);
-
- var pngStream = client.getPngStream();
- pngStream.on('data', console.log);
- arDrone.createClient(['192.168.1.1', 5, null]);
-
-
-
+ });*/
+client.on('navdata', console.log);
+var pngStream = client.getPngStream();
+pngStream.on('data', console.log);
+arDrone.createClient(['192.168.1.1', 5, null]);
--- detectTheFace.py
+++ detectTheFace.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python
 import numpy as np
 import cv2
+import time
 
 #opening text file to write to
 coordinatesFile = open("coordinates.txt", "w+", 0)
-
 #loading classifiers
-face_cascade = cv2.CascadeClassifier('C:\Users\drago\Downloads\Binaries\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:\Users\drago\Downloads\Binaries\opencv\sources\data\haarcascades\haarcascade_eye.xml')
-
-#reading file
-img = cv2.imread('C:/Python27/face.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-     rect = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-     coordinatesFile.write(str(x+w/2))
-     coordinatesFile.write(" ")
-     coordinatesFile.write(str(y+h/2))
-     coordinatesFile.write("\n")
-     roi_gray = gray[y:y+h, x:x+w]
-     roi_color = img[y:y+h, x:x+w]
-     #eyes = eye_cascade.detectMultiScale(roi_gray)
-     #for (ex,ey,ew,eh) in eyes:
-     #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-coordinatesFile.close();
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+while True:
+     coordinatesFile = open("coordinates.txt", "w+", 0)
+     time.sleep(0.1);#sleeps for 100ms
+     face_cascade = cv2.CascadeClassifier('C:\Users\drago\Downloads\Binaries\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
+     eye_cascade = cv2.CascadeClassifier('C:\Users\drago\Downloads\Binaries\opencv\sources\data\haarcascades\haarcascade_eye.xml')
+     #reading file
+     img = cv2.imread('C:/Users/drago/Github/Microshadow/frame.png')
+     if img is None:
+          continue
+     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+     faces = face_cascade.detectMultiScale(gray, 1.3, 3)
+     for (x,y,w,h) in faces:
+          rect = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+          coordinatesFile.write(str(x+w/2))
+          coordinatesFile.write(" ")
+          coordinatesFile.write(str(y+h/2))
+          coordinatesFile.write("\n")
+          roi_gray = gray[y:y+h, x:x+w]
+          roi_color = img[y:y+h, x:x+w]
+          #eyes = eye_cascade.detectMultiScale(roi_gray)
+          #for (ex,ey,ew,eh) in eyes:
+          #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+     cv2.imshow('img',img)
+     cv2.waitKey(0)
+     cv2.destroyAllWindows()          
+     coordinatesFile.close();
